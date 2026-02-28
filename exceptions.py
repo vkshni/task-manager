1da471319b99227a62059854fea7540aef86d459
vkshni <vksdrive24@gmail.com>
2026-02-28 19:58:41 +0530

    Custom exceptions

---
+++ exceptions.py
@@ -0,0 +1,23 @@
+class TaskNotFoundError(Exception):
+
+    pass
+
+
+class FilterNotFoundError(Exception):
+
+    pass
+
+
+class FieldEmptyError(Exception):
+
+    pass
+
+
+class StateTransitionError(Exception):
+
+    pass
+
+
+class CompletedTimeError(Exception):
+
+    pass

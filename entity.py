14f397b0aec502b0fab7c2db1ff8293a7116a515
vkshni <vksdrive24@gmail.com>
2026-02-28 16:08:11 +0530

    validation, serialization and deserialization

--- entity.py
+++ entity.py
@@ -1,26 +1,48 @@
 from uuid import uuid4
+from datetime import datetime
+from state_machine import StateMachine
 
 
 class Task:
 
     def __init__(
-        self, title, created_at, completed_at, task_id=None, note="", state="TODO"
+        self,
+        title,
+        created_at=None,
+        completed_at=None,
+        task_id=None,
+        note="",
+        state="TODO",
     ):
 
-        self.task_id = str(task_id) if task_id else uuid4()
+        self.task_id = str(task_id) if task_id else str(uuid4())
         self.title = title
-        self.created_at = created_at
-        self.completed_at = completed_at
+        self.created_at = (
+            datetime.strptime(created_at, "%d-%m-%Y %H:%M:%S")
+            if created_at
+            else datetime.now()
+        )
+        self.completed_at = (
+            datetime.strptime(completed_at, "%d-%m-%Y %H:%M:%S")
+            if completed_at
+            else None
+        )
         self.note = note
         self.state = state
+
+        self.validate_fields()
 
     def to_list(self):
 
         return [
             self.task_id,
             self.title,
-            self.created_at,
-            self.completed_at,
+            self.created_at.strftime("%d-%m-%Y %H:%M:%S"),
+            (
+                self.completed_at.strftime("%d-%m-%Y %H:%M:%S")
+                if self.completed_at
+                else ""
+            ),
             self.note,
             self.state,
         ]
@@ -36,3 +58,17 @@ class Task:
             task_dict["note"],
             task_dict["state"],
         )
+
+    def validate_fields(self):
+
+        if not self.title or self.title.isspace():
+            raise ValueError("Field cannot be empty")
+
+        if self.state not in StateMachine.transitions.keys():
+            raise ValueError("Invalid state")
+
+        if not self.completed_at and self.state == "DONE":
+            raise ValueError("Completed time should be created when task 'DONE'")
+
+        if self.state != "DONE" and self.completed_at:
+            raise ValueError("Cannot create Completed time when state is not 'DONE'")
